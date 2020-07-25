ba731c67a6f6de0db5fdebfcae68031d1a83b6ec
pyBae-py <sajeel.ahmed@protonmail.com>
2020-07-25 15:15:41 +0500

    :tada: Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, Button, Text } from 'react-native'
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleCount = () => {
+    setCount(count++)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        padding: 50,
+      }}
+    >
+      <Text
+        style={{
+          padding: 15,
+          textAlign: 'center',
+        }}
+      >
+        {count}
+      </Text>
+      <Button
+        onPress={() => {
+          setCount(count + 1)
+        }}
+        title={'+'}
+      />
+      <Text></Text>
+      <Button
+        onPress={() => {
+          if (count === 0) {
+            alert("I can't decrement more")
+            return
+          }
+          setCount(count - 1)
+        }}
+        title={'-'}
+      />
+      <Text></Text>
+      <Button
+        onPress={() => {
+          setCount(0)
+        }}
+        title={'Reset'}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
